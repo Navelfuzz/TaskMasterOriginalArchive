c8fa4c33eefe8024b0fc48485a265cc66908bc4e
Jonathon Stillson <jcstillson77@gmail.com>
2023-09-05 06:00:53 -0400

    trying to fix this mess...

--- app/src/main/java/com/navelfuzz/taskmaster/Activities/AddTaskActivity.java
+++ app/src/main/java/com/navelfuzz/taskmaster/Activities/AddTaskActivity.java
@@ -1,4 +1,27 @@
-package com.navelfuzz.taskmaster.activities;
+package com.navelfuzz.taskmaster.Activities;
 
-public class AddTaskActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.navelfuzz.taskmaster.R;
+
+public class AddTaskActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_task);
+
+        setupAddTaskButton();
+    }
+
+    void setupAddTaskButton(){
+        Button addTaskButton = findViewById(R.id.AddTaskInputButton);
+        addTaskButton.setOnClickListener(view -> {
+            ((TextView)findViewById(R.id.AddTaskSubmittedTextView)).setText("Submitted!");
+        });
+    }
 }
--- app/src/main/java/com/navelfuzz/taskmaster/MainActivity.java
+++ app/src/main/java/com/navelfuzz/taskmaster/MainActivity.java
@@ -2,7 +2,13 @@ package com.navelfuzz.taskmaster;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+
+import com.navelfuzz.taskmaster.Activities.AddTaskActivity;
+import com.navelfuzz.taskmaster.Activities.AllTasksActivity;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setupAddTaskButton();
+        setupAllTasksButton();
+
+    }
+
+    void setupAddTaskButton(){
+        Button addTaskButton = findViewById(R.id.MainActivityAddTaskButton);
+        addTaskButton.setOnClickListener(view -> {
+            Intent goToAddTasksIntent = new Intent(MainActivity.this, AddTaskActivity.class);
+            startActivity(goToAddTasksIntent);
+        });
+    }
+
+    void setupAllTasksButton(){
+        Button allTasksButton = findViewById(R.id.MainActivityAllTasksButton);
+        allTasksButton.setOnClickListener(view -> {
+            Intent goToAllTasksIntent = new Intent(MainActivity.this, AllTasksActivity.class);
+            startActivity(goToAllTasksIntent);
+        });
     }
 }
